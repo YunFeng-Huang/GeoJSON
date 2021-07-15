eb83f7f8e4896f4cf3bef4a81e8ef3e2b53cc514
yunfeng <yunfengjz@qq.com>
2021-07-15 09:27:52 +0800

    save

--- js/heatmap.js
+++ js/heatmap.js
@@ -3,17 +3,31 @@ var layer = new Loca.HeatmapLayer({
 });
 
 
-// layer.setOptions({
-//     style: {
-//         radius: 50,
-//         color: {
-//             0.5: '#2c7bb6',
-//             0.65: '#abd9e9',
-//             0.7: '#ffffbf',
-//             0.9: '#fde468',
-//             1.0: '#d7191c'
-//         }
-//     }
-// });
+function _setlayer(geoJSON){
+    let filter_features = geoJSON.features.filter(v => {
+        v.height = 0;
+        if (v.geometry.coordinates && !Array.isArray(v.geometry.coordinates[0])) {
+            v.height = v.geometry.coordinates[2] || 0;
+        }
+        return v.geometry.type == 'Point';
+    })
 
-// layer.render();
+    layer.setData(filter_features, {
+        lnglat: (data) => {
+            const coordinates = data.value.geometry.coordinates;
+            return [coordinates[0], coordinates[1]];
+        },
+        value: "height",
+    }).setOptions({
+        style: {
+            radius: 16,
+            color: {
+                0.5: '#2c7bb6',
+                0.65: '#abd9e9',
+                0.7: '#ffffbf',
+                0.9: '#fde468',
+                1.0: '#d7191c'
+            }
+        }
+    }).render();
+}
